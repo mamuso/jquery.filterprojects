374d646495ce8967df6f6a3ef6cd36932f529181
“mamuso” <mamuso@mamuso.net>
2009-11-10 23:49:01 +0100

    testing configurations

--- jquery.filterprojects.js
+++ jquery.filterprojects.js
@@ -1,9 +1,9 @@
 (function($) {
-  
+
   $.fn.randomize = function(){
     return $(this).sort(function() {return 0.5 - Math.random()});
   }
-  
+
   $.fn.filterprojects = function(settings) {
     settings = $.extend({
       animationSpeed: 900,
@@ -15,70 +15,68 @@
       show: { width: "show", opacity: "show" },
       hide: { width: "hide", opacity: "hide" },
       filterTagSelector: [] // specify at least one 
-    }, settings);
-          
-    $(this).each(function(i, o){
-      var _groups = [];
-      var _elements = $(this).children();
-      
-      /* Binding the filter */
-      $(this).bind("filter", function(){
-        var _filtered_elements = _elements;
+      }, settings);
+
+      $(this).each(function(i, o){
+        var _elements = $(this).children();
+
+        /* Binding the filter */
+        $(this).bind("filter", function(){
+          var _groups = [];
+          var _filtered_elements = _elements;
+          $.each(settings.filterTagSelector, function(k, j){
+            _groups[k] = [];
+            $(this + "." + settings.activeClass).each(function(){ 
+              if(!$(this).hasClass(settings.allTag) && this.hash != undefined) { _groups[k].push(this.hash.substring(1)); }
+            });
+            if(_groups[k].length > 0){
+              _filtered_elements = _filtered_elements.filter("." + _groups[k].join(",.")); 
+            }
+          });
+
+          /* Randomize */
+          if(settings.randomize){
+            _filtered_elements = _filtered_elements.randomize();
+            _elements = _elements.randomize();
+          }
+
+          /* Show */
+          _filtered_elements.each(function(i,o){
+            $(this).queue(function(){
+              $(this).animate({left: "+0"}, (settings.animationPulse*i)); // dirty trick :)
+              $(this).animate(settings.show, settings.animationSpeed);
+              $(this).dequeue()
+            });
+          });
+
+          /* Hide */
+          _elements.not(_filtered_elements).each(function(i,o){
+            $(this).queue(function(){
+              $(this).animate({left: "+0"}, (settings.animationPulse*i)); // dirty trick :)
+              $(this).animate(settings.hide, settings.animationSpeed);
+              $(this).dequeue()
+            });
+          });
+
+        });
+
+        /* Setup filter selectors */
         $.each(settings.filterTagSelector, function(k, j){
-          _groups[k] = [];
-          $(this + "." + settings.activeClass).each(function(){ 
-            if(!$(this).hasClass(settings.allTag) && this.hash != undefined) { _groups[k].push(this.hash.substring(1)); }
-          });
-          if(_groups[k].length > 0){
-             _filtered_elements = _filtered_elements.filter("." + _groups[k].join(",.")); 
-          }
+          $(""+this).click(function(e){
+            e.preventDefault();
+            if($(this).hasClass(settings.allTag)){
+              $(j).removeClass(settings.activeClass);
+              $(this).addClass(settings.activeClass);
+            } else {
+              $(this).hasClass(settings.activeClass) ? $(this).removeClass(settings.activeClass) : $(this).addClass(settings.activeClass);
+              $(j+"."+settings.activeClass).length > 0 ? $(j+"."+settings.allTag).removeClass(settings.activeClass) : $(j+"."+settings.allTag).addClass(settings.activeClass);
+            }
+            /* Triggering the filter */ 
+            $(o).trigger("filter");
+          })
         });
-        
-        /* Randomize */
-        if(settings.randomize){
-          _filtered_elements = _filtered_elements.randomize();
-          _elements = _elements.randomize();
-        }
-        
-        /* Show */
-        _filtered_elements.each(function(i,o){
-          $(this).queue(function(){
-            $(this).animate({left: "+0"}, (settings.animationPulse*i)); // dirty trick :)
-            $(this).animate(settings.show, settings.animationSpeed);
-            $(this).dequeue()
-          });
-        });
-
-        /* Hide */
-        _elements.not(_filtered_elements).each(function(i,o){
-          $(this).queue(function(){
-            $(this).animate({left: "+0"}, (settings.animationPulse*i)); // dirty trick :)
-            $(this).animate(settings.hide, settings.animationSpeed);
-            $(this).dequeue()
-          });
-        });
-
       });
-      
-      /* Setup filter selectors */
-      $.each(settings.filterTagSelector, function(k, j){
-        $(""+this).click(function(e){
-          e.preventDefault();
-          if($(this).hasClass(settings.allTag)){
-            $(j).removeClass(settings.activeClass);
-            $(this).addClass(settings.activeClass);
-          } else {
-            $(this).hasClass(settings.activeClass) ? $(this).removeClass(settings.activeClass) : $(this).addClass(settings.activeClass);
-            $(j+"."+settings.activeClass).length > 0 ? $(j+"."+settings.allTag).removeClass(settings.activeClass) : $(j+"."+settings.allTag).addClass(settings.activeClass);
-          }
-          /* Triggering the filter */ 
-          $(o).trigger("filter");
-        })
-      });
-      
-    });
+      return this
+    };
     
-    return this
-    
-  };
 })(jQuery);
